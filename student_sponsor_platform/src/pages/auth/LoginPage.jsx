83e583077f5f1988ec53151b5bb93e42a1cb6909
Swati Shende <swatishende@Swatis-MacBook-Air.local>
2026-03-19 23:44:00 -0400

    changes

--- student_sponsor_platform/src/pages/auth/LoginPage.jsx
+++ student_sponsor_platform/src/pages/auth/LoginPage.jsx
@@ -1,4 +1,5 @@
 // src/pages/auth/LoginPage.jsx
+//@author sshende
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
